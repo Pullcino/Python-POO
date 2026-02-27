eb904d0356ee238b9484fbb3d90252f07475496b
Arthur Pulcino <apulcino8@gmail.com>
2026-02-26 21:38:43 -0300

    creating the loop

--- List manipulator/List manipulator.py
+++ List manipulator/List manipulator.py
@@ -41,4 +41,71 @@ class List_manipulator():
         number = int(input("Digit the number you want to add: "))
 
         self.list.append(number)
-        
+
+        print(f"{number} added with success")
+
+    def remove_elements(self):
+
+        try:
+
+            number = int(input("Digit the number you want to remove: "))
+
+            self.list.remove(number)
+
+            print(f"{number} removed with success")
+
+        except ValueError:
+
+            print("The list dont have any item\n")
+
+
+    def found_max(self):
+
+        print(f"The biggest number in the list is: {max(self.list)}")
+
+    def found_min(self):
+
+        print(f"The smallest number in the list is: {min(self.list)}")
+
+    def found_media(self):
+
+        value = sum(self.list)
+
+        media = value / len(self.list)
+
+        print(f"The media is {media}")
+
+    def show_list(self):
+
+        print(f"Thats the list: {self.list}")
+
+
+def main():
+
+    call = List_manipulator()
+
+    while True:
+
+
+        print("Welcome to the list manipulator\n")
+        print("1 - Add a number.")
+        print("2 - Remove a number.")
+        print("3 - Found max value.")
+        print("4 - Found min value.")
+        print("5 - Found list media.")
+        print("6 - Show list.")
+        print("7 - Leave program.\n")
+
+        choice = input("Choose one of the options.")
+
+        if choice == '1':
+
+            call.add_elements()
+
+        elif choice == '2':
+
+            call.remove_elements()
+
+        elif choice == '3':
+
+            call.
